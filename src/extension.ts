66c3d947b03adef4048a27a2ccb4798cfde80591
Allyedge <alimarslank@gmail.com>
2022-12-30 13:33:24 +0100

    v0.0.4

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,12 @@
 import * as vscode from "vscode";
+import { provider } from "./completionProvider";
 
 export function activate(context: vscode.ExtensionContext) {
   let testCommand = vscode.commands.registerCommand("hpl.testCommand", () => {
     vscode.window.showInformationMessage("HPL is working!");
   });
 
-  context.subscriptions.push(testCommand);
+  context.subscriptions.push(testCommand, provider);
 }
 
 export function deactivate() {}
